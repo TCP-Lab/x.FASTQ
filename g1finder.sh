b5d3bf7d50a83748db1a2a7181b964103bc29c78
Federico Alessandro Ruffinatti <federicoalessandro.ruffinatti@unito.it>
2023-04-21 19:43:59 +0200

    Made '--help' as function and prototyped the '--destroy' mode

--- g1finder.sh
+++ g1finder.sh
@@ -4,57 +4,68 @@
 #  Seek and Destroy Google Drive's (1)s 
 # ======================================
 
-# Flag regex pattern (FRP)
+function _help_g1 {
+	echo
+	echo "Seek and destroy the annoying '(1)s' put in filenames by Google Drive"
+	echo
+	echo "Usage: $0 [-h | --help] [-s | --seek] [-d | --destroy] TARGET FNAMES"
+	echo
+	echo "Positional options:"
+	echo "    -h | --help     show this help"
+	echo "    -s | --seek     search mode"
+	echo "    -d | --destroy  cleaning mode"
+	echo "    TARGET          the Google Drive folder to be scanned or fixed"
+	echo "    FNAMES          the output directory for filename report (s-mode)"
+	echo "                    or the input list of filenames to clean (d-mode)"
+	echo
+	echo "Examples: "$0" -s /mnt/e/UniTo\ Drive/ ~"
+	echo "          "$0" -d /mnt/e/UniTo\ Drive/ ./gd1list.txt"
+	echo
+}
+
+# Flag Regex Pattern (FRP)
 frp="^-{1,2}[a-z]+$"
 
+# Argument check
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
-
-	# Make this a function...
-	printf "\nSeek and destroy annoying Google Drive's (1)s in filenames\n"
-	printf "\n"
-	printf "Usage: "$0" -s TARGET_DIR OUT_DIR\n"
-	printf "       "$0" -d TARGET_DIR IN_FILE\n"
-	printf "\n"
-	printf "Positional options:\n"
-	printf "\t-s (--seek)\tsearch mode\n"
-	printf "\t-d (--destroy)\tcleaning mode\n"
-	printf "\tTARGET_DIR\tthe Google Drive folder to be scanned or fixed\n"
-	printf "\tOUT_DIR\t\toutput directory for search mode report\n"
-	printf "\tIN_FILE\t\tpath of the input list of filenames to clean\n"
-	printf "\n"
-	printf "Examples: "$0" -s /mnt/e/UniTo\ Drive/ ~\n"
-	printf "          "$0" -d /mnt/e/UniTo\ Drive/ ./gd1list.txt\n"
-	printf "\n"
-
+	_help_g1
 	exit 0 # Success exit status
-
 elif [[ $# -ge 3 && "$1" =~ $frp ]]; then
-
 	target="$2"
-	report="$3"
+	fnames="$3"
 else
-
-	printf "Wrong syntax. Use --help (-h) option to see mandatory parameters"
+	printf "Wrong syntax. Use '--help' or '-h' option to see mandatory parameters"
 	exit 1 # Failure exit status
 fi
 
-# The 'base regex pattern' (BRP) is a white-space followed by a one-digit number
-# within round brackets
-brp=" \([1-3]\)"
+# The 'Target Regex Pattern' (TRP) is a white-space followed by a one-digit
+# number (1 to 3) within round brackets
+trp=" \([1-3]\)"
 
 if [[ "$1" == "-s" || "$1" == "--seek" ]]; then
 
-	# Find folders and sub-folders that end with the BRP
-	find "$target" -type d | grep -E ".+$brp$" > "$report"/gd1list.txt
+	# Find folders and sub-folders that end with the TRP
+	find "$target" -type d | grep -E ".+$trp$" > "$fnames"/gd1list.txt
 
-	# Find regular files that end with the BRP, plus a possible filename extension
-	find "$target" -type f | grep -E ".+$brp(\.[a-zA-Z0-9]+)?$" >> "$report"/gd1list.txt
+	# Find regular files that end with the TRP, plus a possible filename extension
+	find "$target" -type f | grep -E ".+$trp(\.[a-zA-Z0-9]+)?$" >> "$fnames"/gd1list.txt
 
-	echo "Number of hits: $(wc -l "$report"/gd1list.txt)"
+	echo -e "\nNumber of hits: $(wc -l "$fnames"/gd1list.txt)"
 
 elif [[ "$1" == "-d" || "$1" == "--destroy" ]]; then
 
-	printf "Still to be implemented..."	
+	printf "Still to be implemented..."
+	# https://www.cyberciti.biz/faq/unix-howto-read-line-by-line-from-file/
+	# https://stackoverflow.com/questions/13210880/replace-one-substring-for-another-string-in-shell-script
+	
+	while IFS= read -r line
+	do
+	  echo ${line/ ([1-3])/""}
+	  #mv "$line" "${line/ ([1-3])/""}"
+	done < "$fnames"/gd1list.txt
+
+	#cat gd1list.txt | sed -r "s/$trp//g"
+
 else
 
 	printf "Invalid flag $1"
