4f8978937a2d2539e1637bde4174e72bb630e921
Federico Alessandro Ruffinatti <federicoalessandro.ruffinatti@unito.it>
2023-07-12 18:53:08 +0200

    In `getfastq.sh`, made `fastq_address` fetching more general. In addition, improved code readability and comments.

--- getfastq.sh
+++ getfastq.sh
@@ -5,9 +5,13 @@
 # ==============================================
 
 # NOTE:
-#	- the for loop instatiates all the downloads in parallel
-#	- use `tail -n 3 *.log` to see their progress
-#	- use `pgrep -l -u fear` to get the IDs of the active wget processes
+#	- The script assumes all the target address are in the input file
+# 	- the script reads line by line
+# 	- because of limitations on FTP, target address are converted to HTTP,
+# 		taking advantage of the intrinsic versatility of ENA browser
+#	- the for loop instantiates all downloads in parallel
+#	- use, e.g., `tail -n 3 *.log` to see their progress
+#	- use, e.g., `pgrep -l -u fear` to get the IDs of the active wget processes
 #
 # ISSUES:
 #	1. write the help option
@@ -16,23 +20,25 @@
 
 while IFS= read -r line
 do
-	# using Bash-native string substitution to change FTP into HTTP
+	# Using Bash-native string substitution syntax to change FTP into HTTP
 	# ${string/$substring/$replacement}
 	# NOTE: while `$substring` and `$replacement` are literal strings
 	# 		the starting `string` MUST be a reference to a variable name!
 	target=${line/ftp:/http:}
 
-	base_line="$(basename "$target")"
+	fastq_name="$(basename "$target")"
+	fastq_address="$(dirname ${target/wget* /})"
 
-	# nohup (no hangups)  To keep processes running even after exiting the shell
-	# per reindirizzare lo standard output e l'errore standard sul file di log
-	nohup $target > ${base_line}.log 2>&1 &
+	# `nohup` (no hangups) allows keeping processes running even after exiting
+	# the shell (`2>&1` is used to redirect both the standard output and the
+	# standard error to the FASTQ-specific log file).
+	nohup $target > ${fastq_name}.log 2>&1 &
 
 	# Toggle verbose on-screen logging
 	if true; then
 		echo
-		echo "Downloading: $(basename "$target")"
-		echo "From       : $(dirname ${target/"wget -nc"/})"
+		echo "Downloading: $fastq_name"
+		echo "From       : $fastq_address"
 	fi
 	
 done < "$1"
