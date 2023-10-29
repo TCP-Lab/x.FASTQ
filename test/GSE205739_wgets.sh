e997567294f39070ba04d620e8c651e0d17a2a67
Federico Alessandro Ruffinatti <federicoalessandro.ruffinatti@unito.it>
2023-10-29 11:18:05 +0100

    Changed test series GSE138309 to GSE205739 to have 4 samples instead of three.

---
+++ test/GSE205739_wgets.sh
@@ -0,0 +1,8 @@
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR195/066/SRR19592966/SRR19592966_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR195/064/SRR19592964/SRR19592964_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR195/063/SRR19592963/SRR19592963_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR195/065/SRR19592965/SRR19592965_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR195/066/SRR19592966/SRR19592966_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR195/065/SRR19592965/SRR19592965_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR195/064/SRR19592964/SRR19592964_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR195/063/SRR19592963/SRR19592963_2.fastq.gz
