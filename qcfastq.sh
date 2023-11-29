113f1399071275354ef5c00a2be411b2f4a58d2a
Federico Alessandro Ruffinatti <federicoalessandro.ruffinatti@unito.it>
2023-11-30 00:53:21 +0100

    `qcfastq.sh`> Fixed issues coming from handling of spaces in path and filenames (see both _progress_qcfastq function and FastQC call). In addition, many small changes to help and comments.

--- qcfastq.sh
+++ qcfastq.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# ============================================================================ #
-#  FASTQ Quality Control
-# ============================================================================ #
+# ==============================================================================
+#  Quality Control Tools for NGS Data
+# ==============================================================================
 
 # --- General settings and variables -------------------------------------------
 
@@ -12,7 +12,7 @@ set -u # "no-unset" shell option
 # --- Function definition ------------------------------------------------------
 
 # Default options
-ver="1.4.1"
+ver="1.4.2"
 verbose=true
 tool="FastQC"
 
@@ -27,23 +27,23 @@ function _help_qcfastq {
 	echo
 	echo "This script is meant to perform Quality Control (QC) analyses of NGS"
 	echo "data by wrapping some of the most popular QC software tools currently"
-	echo "around (e.g, FastQC, MultiQC, QualiMap). Specifically, 'qcfastq' runs"
+	echo "around (e.g, FastQC, MultiQC, QualiMap). Specifically, qcFASTQ runs"
 	echo "them persistently (by 'nohup') and in background, possibly cycling"
 	echo "over multiple input files."
 	echo
 	echo "Usage:"
 	echo "  qcfastq [-h | --help] [-v | --version]"
-	echo "  qcfastq -p | --progress [TARGETS]"
+	echo "  qcfastq -p | --progress [DATADIR]"
 	echo "  qcfastq [-q | --quiet] [--suffix=STRING] [--tool=QCTYPE]"
-	echo "          [--out=NAME] TARGETS"
+	echo "          [--out=NAME] DATADIR"
 	echo
 	echo "Positional options:"
-	echo "  -h | --help      Show this help."
-	echo "  -v | --version   Show script's version."
-	echo "  -p | --progress  Show QC analysis progress by 'tailing' the latest"
-	echo "                   (possibly still growing) QC log. If TARGETS is not"
-	echo "                   specified, search \$PWD for QC logs."
-	echo "  -q | --quiet     Disable verbose on-screen logging."
+	echo "  -h | --help      Shows this help."
+	echo "  -v | --version   Shows script's version."
+	echo "  -p | --progress  Shows QC analysis progress by 'tailing' the latest"
+	echo "                   (possibly still growing) QC log. If DATADIR is not"
+	echo "                   specified, it searches \$PWD for QC logs."
+	echo "  -q | --quiet     Disables verbose on-screen logging."
 	echo "  --suffix=STRING  A string specifying the suffix (e.g., a filename"
 	echo "                   extension) used by qcFASTQ for selecting the files"
 	echo "                   to analyze. The default for FastQC is \".fastq.gz\","
@@ -55,16 +55,16 @@ function _help_qcfastq {
 	echo "                   the user and made globally available. As an"
 	echo "                   alternative, they can be placed in any directory"
 	echo "                   (even if not included in \$PATH) and made visible"
-	echo "                   to qcfastq only by editing 'install.paths'."
+	echo "                   only to qcFASTQ by editing 'install.paths' file."
 	echo "  --out=NAME       The name of the output folder. The default name is"
 	echo "                   \"QCTYPE_out\". Only a folder NAME is required,"
 	echo "                   not its entire path; if a full path is provided,"
 	echo "                   only its 'basename' will be used. In any case, the"
 	echo "                   script will attempt to create a new folder as a"
-	echo "                   sub-directory of the TARGETS folder; if it already"
+	echo "                   sub-directory of the DATADIR folder; if it already"
 	echo "                   exists the whole process is aborted to avoid"
-	echo "                   overwriting possible previous reports."
-	echo "  TARGETS          The path to the folder containing the files to"
+	echo "                   possible overwriting of previous reports."
+	echo "  DATADIR          The path to the folder containing the files to"
 	echo "                   be analyzed. Unlike MultiQC, FastQC and PCA are"
 	echo "                   not designed to search sub-directories."
 	echo
@@ -83,19 +83,22 @@ function _progress_qcfastq {
 	if [[ -d "$1" ]]; then
 		target_dir="$1"
 	else
-		printf "Bad TARGETS path '$1'.\n"
+		printf "Bad DATADIR path '$1'.\n"
 		exit 2 # Argument failure exit status: bad target path
 	fi
 
 	# NOTE: In the 'find' command below, the -printf "%T@ %p\n" option prints
 	#       the modification timestamp followed by the filename.
-	latest_log=$(find "${target_dir}" -maxdepth 1 -type f -iname "Z_QC_*.log" \
-		-printf "%T@ %p\n" | sort -n | tail -n 1 | cut -d " " -f 2)
+	#       The '-f 2-' option in 'cut' is used to take all the fields except
+	#       the first one (i.e., the timestamp) to properly handle filenames
+	#       or paths with spaces.
+	latest_log="$(find "${target_dir}" -maxdepth 1 -type f -iname "Z_QC_*.log" \
+		-printf "%T@ %p\n" | sort -n | tail -n 1 | cut -d " " -f 2-)"
 
 	if [[ -n "$latest_log" ]]; then
 		
 		tool=$(basename "$latest_log" \
-			| sed -E "s/^Z_QC_//" | sed -E "s/_.*\.log$//")
+			| sed "s/^Z_QC_//" | sed "s/_.*\.log$//")
 		printf "\n$tool log file detected:\n${latest_log}\n"
 
 		case "$tool" in
@@ -106,7 +109,6 @@ function _progress_qcfastq {
 				printf "\n${grn}Completed:${end}\n"
 				grep --no-filename "Analysis complete" "${latest_log}" \
 					|| [[ $? == 1 ]]
-				
 				printf "\n${yel}Tails:${end}\n"
 				tail -n 1 "${latest_log}"
 			;;
@@ -143,7 +145,7 @@ while [[ $# -gt 0 ]]; do
 				exit 0 # Success exit status
 			;;
 			-p | --progress)
-				# Cryptic one-liner meaning "$2" or $PWD if argument 2 is unset
+				# Cryptic one-liner meaning "$2", or $PWD if argument 2 is unset
 				_progress_qcfastq "${2:-.}"
 			;;
 			-q | --quiet)
@@ -171,12 +173,12 @@ while [[ $# -gt 0 ]]; do
 					# Check if a given string is an element of an array.
 					# NOTE: leading and trailing spaces around array elements
 					#       are used to ensure accurate pattern matching!
-					if [[ " $(_get_qc_tools "names") " == *" ${tool} "* ]]; then
+					if [[ " $(_get_qc_tools names) " == *" ${tool} "* ]]; then
 						shift
 					else
 						printf "Invalid QC tool name: '${tool}'.\n"
 						printf "Please, choose among the following options:\n"
-						for i in $(_get_qc_tools "names"); do
+						for i in $(_get_qc_tools names); do
 							printf "  -  $i\n"
 						done
 						exit 5
@@ -208,20 +210,20 @@ while [[ $# -gt 0 ]]; do
 			;;
 		esac
 	else
-		# The first non-FRP sequence is taken as the TARGETS argument
+		# The first non-FRP sequence is taken as the DATADIR argument
 		target_dir="$1"
 		break
 	fi
 done
 
-# Argument check: TARGETS directory
+# Argument check: DATADIR directory
 if [[ -z "${target_dir:-""}" ]]; then
-	printf "Missing option or TARGETS argument.\n"
+	printf "Missing option or DATADIR argument.\n"
 	printf "Use '--help' or '-h' to see the expected syntax.\n"
-	exit 9 # Argument failure exit status: missing TARGETS
+	exit 9 # Argument failure exit status: missing DATADIR
 elif [[ ! -d "$target_dir" ]]; then
-	printf "Invalid target directory '$target_dir'.\n"
-	exit 10 # Argument failure exit status: invalid TARGETS
+	printf "Invalid target directory '$(realpath "$target_dir")'.\n"
+	exit 10 # Argument failure exit status: invalid DATADIR
 fi
 
 # Argument check: QCTYPE tool
@@ -235,28 +237,27 @@ if which "$(_name2cmd ${tool})" > /dev/null 2>&1; then
 else
 	# Search the 'install.paths' file for it.
 	# NOTE: Mind the final slash! It has to be included in 'tool_path' variable
-	#       so that it does not appear when the command to launch the QC tool is
-	#       globally visible (tool_path="").
+	#       so that it does not appear when calling a globally visible QC tool
+	#       (tool_path="").
 	tool_path="$(grep -i "$(hostname):${tool}:" \
 		"${xpath}/install.paths" | cut -d ':' -f 3)"/
 
 	if [[ ! -f "${tool_path}$(_name2cmd ${tool})" ]]; then
 		printf "${tool} not found...\n"
 		printf "Install ${tool} and update the 'install.paths' file,\n"
-		printf "or make it globally visible creating a link to "
+		printf "or make it globally visible by creating a link to "
 		printf "\'$(_name2cmd ${tool})\' in some \$PATH folder.\n"
-		exit 1 # Argument failure exit status: tool not found
+		exit 11 # Argument failure exit status: tool not found
 	fi
 fi
 
 # --- Main program -------------------------------------------------------------
 
 target_dir="$(realpath "$target_dir")"
-log_file="${target_dir}"/Z_QC_"${tool}"_"$(basename "$target_dir")"_$(_tstamp).log
+log_file="${target_dir}/Z_QC_${tool}_$(basename "$target_dir")_$(_tstamp).log"
 
-# Existence operator ${:-} <=> ${user-defined_name:-default_name}
 output_dir="${target_dir}/${out_dirname:-"${tool}_out"}"
-mkdir "$output_dir" # Stop here if it already exists !!!
+mkdir "$output_dir" # Stop here if it already exists !!! (exit status 1)
 
 _dual_log $verbose "$log_file" "\n\
 	Running ${tool} tool in background
@@ -266,7 +267,7 @@ _dual_log $verbose "$log_file" "\n\
 case "$tool" in
 	PCA)
 		nohup Rscript "${xpath}"/cc_pca.R \
-			"${suffix:=".tsv"}" "${output_dir}" "${target_dir}" \
+			"${suffix:-".tsv"}" "${output_dir}" "${target_dir}" \
 			>> "$log_file" 2>&1 &
 	;;
 	FastQC)
@@ -278,18 +279,16 @@ case "$tool" in
 				Found $counter FASTQ files ending with \"${suffix}\" \
 				in ${target_dir}."
 			
-			target_files=$(find "$target_dir" -maxdepth 1 -type f \
-				-iname "*$suffix")
-			
-			nohup ${tool_path}fastqc -o "${output_dir}" ${target_files} \
-				>> "$log_file" 2>&1 &
+			# FastQC recognizes multiple files with the use of wildcards
+			nohup ${tool_path}fastqc -o "${output_dir}" \
+				"${target_dir}"/*"${suffix}" >> "$log_file" 2>&1 &
 		else
 			_dual_log true "$log_file" "\n\
 				There are no FASTQ files ending with \"${suffix}\" \
 				in ${target_dir}.\n\
 				Stop Execution."
 			rmdir "$output_dir"
-			exit 11 # Argument failure exit status: no FASTQ found
+			exit 12 # Argument failure exit status: no FASTQ found
 		fi
 	;;
 	MultiQC)
