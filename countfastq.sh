9f048afd67d77fdc3f617d2132f8e6cf2e477070
Federico Alessandro Ruffinatti <federicoalessandro.ruffinatti@unito.it>
2024-03-11 19:21:58 +0100

    Updated 'countfastq.sh' and 'assembler.R' versions.

--- countfastq.sh
+++ countfastq.sh
@@ -12,7 +12,7 @@ set -u # "no-unset" shell option
 # --- Function definition ------------------------------------------------------
 
 # Default options
-ver="1.4.2"
+ver="1.4.3"
 verbose=true
 gene_names=false
 level="genes"
