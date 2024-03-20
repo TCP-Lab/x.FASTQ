3c49936e2ef1ea3ad27075b4a001d3c1e8fc7f6c
Federico Alessandro Ruffinatti <federicoalessandro.ruffinatti@unito.it>
2024-03-20 15:39:51 +0100

    ALL BASH FILES: all in dentations have been converted from `TAB`s to 4 space characters. `x.funx.sh`> `_dual_log` function has been rewritten to be robust with respect to the type of indentation. All scripts have been fixed accordingly and tested.

--- trimfastq.sh
+++ trimfastq.sh
@@ -3,7 +3,7 @@
 # ==============================================================================
 #  Persistently Trim FastQ Files using BBDuk
 # ==============================================================================
-ver="1.3.2"
+ver="1.3.3"
 
 # --- Source common settings and functions -------------------------------------
 
@@ -34,19 +34,19 @@ progress=false
 
 # Argument check: override -h, -v, -q, and -p option settings
 for arg in "$@"; do
-	if [[ "$arg" == "-h" || "$arg" == "--help" ]]; then
-		printf "%s\n" "$_help_trimfastq"
-		source "${xpath}"/trimmer.sh --help
-		exit 0 # Success exit status
-	elif [[ "$arg" == "-v" || "$arg" == "--version" ]]; then
-		figlet trim FASTQ
-		printf "Ver.${ver} :: The Endothelion Project :: by FeAR\n"
-		exit 0 # Success exit status
-	elif [[ "$arg" == "-q" || "$arg" == "--quiet" ]]; then
-		verbose=false
-	elif [[ "$arg" == "-p" || "$arg" == "--progress" ]]; then
-		progress=true
-	fi
+    if [[ "$arg" == "-h" || "$arg" == "--help" ]]; then
+        printf "%s\n" "$_help_trimfastq"
+        source "${xpath}"/trimmer.sh --help
+        exit 0 # Success exit status
+    elif [[ "$arg" == "-v" || "$arg" == "--version" ]]; then
+        figlet trim FASTQ
+        printf "Ver.${ver} :: The Endothelion Project :: by FeAR\n"
+        exit 0 # Success exit status
+    elif [[ "$arg" == "-q" || "$arg" == "--quiet" ]]; then
+        verbose=false
+    elif [[ "$arg" == "-p" || "$arg" == "--progress" ]]; then
+        progress=true
+    fi
 done
 
 # --- Main program -------------------------------------------------------------
@@ -54,15 +54,15 @@ done
 running_proc=$(pgrep -l -u "$USER" "bbduk" | wc -l || [[ $? == 1 ]])
 if [[ $running_proc -gt 0 ]] && (! $progress) && $verbose; then
 
-	echo "WARNING: Some instances of BBDuk are already running in the background!"
-	echo "Are you sure you want to continue? (y/n)"
+    echo "WARNING: Some instances of BBDuk are already running in the background!"
+    echo "Are you sure you want to continue? (y/n)"
 
-	# Prompt the user for input
-	read -r response
-	if  [[ "$response" != "y" && "$response" != "Y" ]]; then
-		echo "ABORTING..."
-		exit 0
-	fi
+    # Prompt the user for input
+    read -r response
+    if [[ "$response" != "y" && "$response" != "Y" ]]; then
+        echo "ABORTING..."
+        exit 0
+    fi
 fi
 
 # Get the last argument (i.e., either DATADIR or -p)
@@ -73,13 +73,13 @@ target_dir="${!#}"
 #       last one, which needs special attention to handle possible spaces in
 #       DATADIR path.
 if ${verbose} && (! ${progress}); then
-	echo -e -n\
-		"\nRunning: nohup bash trimmer.sh -q ${@:1:$#-1} \"${target_dir}\" &"
+    echo -e -n\
+        "\nRunning: nohup bash trimmer.sh -q ${@:1:$#-1} \"${target_dir}\" &"
 fi
 
 # MAIN STATEMENT
 nohup bash "${xpath}"/trimmer.sh -q ${@:1:$#-1} "${target_dir}" \
-	> "nohup.out" 2>&1 &
+    > "nohup.out" 2>&1 &
 
 # Allow time for 'nohup.out' to be created
 sleep 0.5
@@ -88,10 +88,10 @@ sleep 0.5
 # show progress when run with the '-p' option. For this reason, only when
 # 'nohup.out' file is empty 'trimmer.sh' is actually going to trim something...
 if [[ -s "nohup.out" ]]; then
-	echo
-	cat "nohup.out" # Retrieve error messages...
-	rm "nohup.out"  # ...and then clean
-	exit 13
+    echo
+    cat "nohup.out" # Retrieve error messages...
+    rm "nohup.out"  # ...and then clean
+    exit 13
 fi
 
 rm "nohup.out"
@@ -99,18 +99,18 @@ rm "nohup.out"
 # Print the head of the log file just created, as a preview of the scheduled job
 if ${verbose} && (! ${progress}); then
 
-	# Allow time for the new log to be created and found
-	sleep 0.5
-	# NOTE: In the 'find' command below, the -printf "%T@ %p\n" option prints
-	#       the modification timestamp followed by the filename.
-	#       The '-f 2-' option in 'cut' is used to take all the fields after
-	#       the first one (i.e., the timestamp) to avoid cropping possible
-	#       filenames or paths with spaces.
-	latest_log="$(find "${target_dir}" -maxdepth 1 -type f \
-		-iname "Z_Trimmer_*.log" -printf "%T@ %p\n" \
-		| sort -n | tail -n 1 | cut -d " " -f 2-)"
+    # Allow time for the new log to be created and found
+    sleep 0.5
+    # NOTE: In the 'find' command below, the -printf "%T@ %p\n" option prints
+    #       the modification timestamp followed by the filename.
+    #       The '-f 2-' option in 'cut' is used to take all the fields after
+    #       the first one (i.e., the timestamp) to avoid cropping possible
+    #       filenames or paths with spaces.
+    latest_log="$(find "${target_dir}" -maxdepth 1 -type f \
+        -iname "Z_Trimmer_*.log" -printf "%T@ %p\n" \
+        | sort -n | tail -n 1 | cut -d " " -f 2-)"
 
-	printf "\n\nHead of ${latest_log}\n"
-	head -n 9 "$latest_log"
-	printf "Start trimming through BBDuk in background...\n"
+    printf "\n\nHead of ${latest_log}\n"
+    head -n 9 "$latest_log"
+    printf "Start trimming through BBDuk in background...\n"
 fi
