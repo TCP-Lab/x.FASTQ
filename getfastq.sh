4930c34f1886964b4e385c89a326949d4c16aca3
Luca Visentin <luca.visentin@unito.it>
2024-04-19 15:09:39 +0200

    fix: add quotes to escape spaces

--- getfastq.sh
+++ getfastq.sh
@@ -342,7 +342,7 @@ else
         fi
 
         # MAIN STATEMENT
-        nohup bash ${xpath}/getcheck.sh "$line" "$checksum" "$(basename "$line")" >> "$log_file" 2>&1 &
+        nohup bash "${xpath}"/getcheck.sh "$line" "$checksum" "$(basename "$line")" >> "$log_file" 2>&1 &
         # Originally, this was 'nohup bash -c "$line"', but it didn't print
         # the 'Terminated' string in the log file when killed by the -k option
         # (thus affecting in turn '_progress_getfastq'). So I used a
